eee2112b118b32f647efc9d680f71ad6a5be8b74
superdev905 <superdev905@gmail.com>
2016-08-20 08:58:04 -0700

    Mobile Tagline

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,5 +2,20 @@ import './index.scss';
 import React from 'react';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="home">
+      <Tagline />
+    </div>
+  );
+}
+
+function Tagline() {
+  return (
+    <div className="home--tagline">
+      <div className="master-react-redux"><span>Master React,</span></div>
+      <div className="master-react-redux"><span>Redux,</span></div>
+      <div className="webpack-es6"><span>Webpack</span></div>
+      <div className="webpack-es6"><span>and ES2016</span></div>
+    </div>
+  );
 }
