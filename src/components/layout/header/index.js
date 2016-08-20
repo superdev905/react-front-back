ec3f72b468e525f547ef1fdff1b9091834e1fe26
superdev905 <superdev905@gmail.com>
2016-08-20 09:27:02 -0700

    Hamburger Menu

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -1,19 +1,20 @@
 import './index.scss';
-import React from 'react';
-import RULogo from './assets/react_university_icon_alt_light.svg';
+import React             from 'react';
+import RULogo            from './assets/react_university_icon_alt_light.svg';
+import { slide as Menu } from 'react-burger-menu'
 
 export default function Header() {
   return (
     <div className="app-header">
       <img src={RULogo} alt="React University Logo" />
       <span className="app-header--name"><a href="/">React University</a></span>
+      <Menu right={true} >
+        <a href="./training" className="menu-item">Training</a>
+        <a href="./books" className="menu-item">Books</a>
+        <a href="./posts" className="menu-item">Posts</a>
+        <a href="./about" className="menu-item">About</a>
+        <a href="./register" className="menu-item">Register</a>
+      </Menu>
     </div>
   );
 }
-        //<ul>
-          //<li><a href="./training" className="app-header-link">Training</a></li>
-          //<li><a href="./books" className="app-header-link">Books</a></li>
-          //<li><a href="./posts" className="app-header-link">Posts</a></li>
-          //<li><a href="./about" className="app-header-link">About</a></li>
-          //<li><a href="./register" className="app-header-register">Register</a></li>
-        //</ul>
