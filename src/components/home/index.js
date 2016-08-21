9ea911575f6728411d92d3051a3962c51f7ef224
superdev905 <superdev905@gmail.com>
2016-08-20 21:23:35 -0700

    Add CTA component

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="home">
       <Tagline />
+      <Cta />
     </div>
   );
 }
@@ -16,6 +17,19 @@ function Tagline() {
       <div className="master-react-redux"><span>Redux,</span></div>
       <div className="webpack-es6"><span>Webpack</span></div>
       <div className="webpack-es6"><span>and ES2016</span></div>
+    </div>
+  );
+}
+
+function Cta() {
+  return (
+    <div className="home--cta">
+      <div className="home--cta--view-courses">
+        <a>View Courses <i className="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+      </div>
+      <div className="home--cta--copy">
+        <p>Accelerate your career by learning from the experts. Master everything you need to <span className="blue-text">build modern JavaScript applications</span> using React, Redux, Webpack, WebSockets, WebRTC, Node and more.</p>
+      </div>
     </div>
   );
 }
