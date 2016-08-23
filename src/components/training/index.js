b01c69285887ba72184efcbdb34686e3b33a6421
superdev905 <superdev905@gmail.com>
2016-08-23 11:13:53 -0700

    Add Newsletter and Footer

--- src/components/training/index.js
+++ src/components/training/index.js
@@ -8,6 +8,8 @@ export default function Training() {
   		<Workshop1 />
   		<Workshop2 />
   		<Corporate />
+  		<Newsletter />
+  		<Footer />
   	</div>
   );
 }
@@ -61,4 +63,38 @@ function Corporate() {
 			<p>for more information.</p>
 		</div>
 	);
+}
+
+function Newsletter () {
+  return (
+    <div className="training--newsletter">
+      <div className="training--newsletter--title">
+        <h2>Accelerate your career with React</h2>
+      </div>
+      <p>Get notified about future workshops</p>
+      <div id="mc_embed_signup">
+        <form action="//reactuniversity.us12.list-manage.com/subscribe/post?u=b2288653d99f8d9216e217796&amp;id=5079f900cd" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+          <div id="mc_embed_signup_scroll">
+            <input type="email" name="EMAIL" className="subscriber-email" id="mce-EMAIL" placeholder="email address" required />
+            <div style={{"position": "absolute", "left": "-5000px", "ariaHidden": true}}>
+              <input type="text" name="b_b2288653d99f8d9216e217796_5079f900cd" tabIndex="-1"  />
+            </div>
+            <input type="submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" className="btn-register" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+function Footer () {
+  return (
+    <div className="training--footer">
+      <div className="training--footer--social">
+        <a href="https://twitter.com/thereactu"><i className="fa fa-twitter"></i></a>
+        <a href="https://www.facebook.com/reactuniversity"><i className="fa fa-facebook-f"></i></a>
+      </div>
+      <p>Copyright { '\u00A9' } 2016 React University</p>
+    </div>
+  );
 }
