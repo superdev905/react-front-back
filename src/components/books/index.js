9d336fd99ae7a276dcb0e796682586399f678c38
superdev905 <superdev905@gmail.com>
2016-08-23 18:43:55 -0700

    Add Ipad Section

--- src/components/books/index.js
+++ src/components/books/index.js
@@ -1,10 +1,12 @@
 import './index.scss';
 import React from 'react';
+import Ebook  from './assets/ebook.svg';
 
 export default function Books() {
   return (
   	<div className="books">
   		<Tagline />
+  		<Ipad />
   	</div>
   ); 
 }
@@ -15,4 +17,16 @@ function Tagline() {
       <h2>Books</h2>
     </div>
   );
+}
+
+function Ipad() {
+	return (
+		<div className="books--ipad">
+			<img src={Ebook} alt="Ebook" />
+			<div className="books--ipad--desc">
+				<p>These books are great additions to our in-person training. Some are available for free to attendees of our workshops.</p>
+				<hr></hr>
+			</div>
+		</div>
+	);
 }
