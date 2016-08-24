1a72b2f5f219c833a8543385ac3fbd95b28ef6e2
superdev905 <superdev905@gmail.com>
2016-08-23 18:10:57 -0700

    Add books/index.scss and Tagline

--- src/components/books/index.js
+++ src/components/books/index.js
@@ -1,5 +1,18 @@
+import './index.scss';
 import React from 'react';
 
 export default function Books() {
-  return <h1>Books</h1>;
+  return (
+  	<div className="books">
+  		<Tagline />
+  	</div>
+  ); 
+}
+
+function Tagline() {
+  return (
+    <div className="books--tagline">
+      <h2>Books</h2>
+    </div>
+  );
 }
