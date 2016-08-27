e672830b943b04faff62fca71247124d39589474
superdev905 <superdev905@gmail.com>
2016-08-27 15:22:19 -0700

    Responsive Homepage and Header

--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -4,11 +4,15 @@ import React from 'react';
 export default function Footer () {
   return (
     <div className="footer">
-      <div className="home--footer--social">
-        <a href="https://twitter.com/thereactu" target="_blank"><i className="fa fa-twitter"></i></a>
-        <a href="https://www.facebook.com/reactuniversity" target="_blank"><i className="fa fa-facebook-f"></i></a>
+      <div className="row">
+        <div className="footer--social medium-2 medium-push-9 columns">
+          <a href="https://twitter.com/thereactu" target="_blank"><i className="fa fa-twitter"></i></a>
+          <a href="https://www.facebook.com/reactuniversity" target="_blank"><i className="fa fa-facebook-f"></i></a>
+        </div>
+        <p className="footer--copyright medium-5 medium-pull-6 columns">
+          Copyright { '\u00A9' } 2016 React University
+        </p>
       </div>
-      <p>Copyright { '\u00A9' } 2016 React University</p>
     </div>
   );
 }
--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -3,15 +3,17 @@ import React             from 'react';
 import RULogo            from './assets/react_university_icon_alt_light.svg';
 import { slide as Menu } from 'react-burger-menu'
 
-export default function Header({ isMobile }) {
-  const menu = isMobile ? <BurgerMenu /> : <DesktopMenu />
+export default function Header() {
   return (
     <div className="app-header">
-      <div className="app-header--logo-container">
-        <img src={RULogo} alt="React University Logo" />
-        <span className="app-header--name"><a href="/">React University</a></span>
+      <div className="row">
+        <div className="app-header--logo-container medium-4 columns">
+          <img src={RULogo} alt="React University Logo" />
+          <span className="app-header--name"><a href="/">React University</a></span>
+        </div>
+        <BurgerMenu />
+        <DesktopMenu />
       </div>
-      { menu }
     </div>
   );
 }
@@ -31,13 +33,16 @@ function BurgerMenu() {
 
 function DesktopMenu() {
   return (
-    <div className="app-header--navigation">
-      <a href="/training" className="menu-item"><i className="fa fa-code" aria-hidden="true"></i> Training</a>
-      <a href="/books" className="menu-item"><i className="fa fa-book" aria-hidden="true"></i> Books</a>
-      <a href="/posts" className="menu-item"><i className="fa fa-commenting" aria-hidden="true"></i> Posts</a>
-      <a href="/about" className="menu-item"><i className="fa fa-info-circle" aria-hidden="true"></i> About</a>
-      <a href="/register" className="menu-item"><i className="fa fa-user-plus" aria-hidden="true"></i> Register</a>
-      <a href="/" className="menu-item"><i className="fa fa-home" aria-hidden="true"></i> Home</a>
+    <div className="app-header--navigation medium-8 columns">
+      <span className="non-register">
+        <a href="/training" className="menu-item">Training</a>
+        <a href="/books" className="menu-item">Books</a>
+        <a href="/posts" className="menu-item">Posts</a>
+        <a href="/about" className="menu-item">About</a>
+      </span>
+      <span className="register">
+        <a href="/register" className="menu-item">Register</a>
+      </span>
     </div>
   );
 }
