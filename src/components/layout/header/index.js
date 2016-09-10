f4ecb3ab7b2424a649ef3d2ec2b29b8c10bc02a9
superdev905 <superdev905@gmail.com>
2016-09-09 20:56:04 -0700

    Fix burger menu refresh

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -1,15 +1,18 @@
 import './index.scss';
 import React             from 'react';
-import RULogo            from './assets/react_university_icon_alt_light.svg';
 import { slide as Menu } from 'react-burger-menu'
+import { Link }          from 'react-router';
+import Radium            from 'radium';
+
+const RadiumLink = Radium(Link);
 
 export default function Header() {
   return (
     <div className="app-header">
       <div className="row">
         <div className="app-header--logo-container medium-4 columns">
-          <img src={RULogo} alt="React University Logo" />
-          <span className="app-header--name"><a href="/">React University</a></span>
+          <img src={require('./assets/react_university_icon_alt_light.svg')} alt="React University Logo" />
+          <span className="app-header--name"><Link to="/">React University</Link></span>
         </div>
         <BurgerMenu />
         <DesktopMenu />
@@ -19,14 +22,25 @@ export default function Header() {
 }
 
 function BurgerMenu() {
+  const styles = {
+    bmMenuWrap: {
+      zIndex: '3'
+    },
+    bmOverlay: {
+      height: 'calc(100% + 35px)'
+    },
+    bmMenu: {
+      height: 'calc(100% + 35px)'
+    }
+  };
   return (
-    <Menu right={true} >
-      <a href="/training" className="menu-item"><i className="fa fa-code" aria-hidden="true"></i> Training</a>
-      <a href="/books" className="menu-item"><i className="fa fa-book" aria-hidden="true"></i> Books</a>
-      <a href="/posts" className="menu-item"><i className="fa fa-commenting" aria-hidden="true"></i> Posts</a>
-      <a href="/about" className="menu-item"><i className="fa fa-info-circle" aria-hidden="true"></i> About</a>
-      <a href="/register" className="menu-item"><i className="fa fa-user-plus" aria-hidden="true"></i> Register</a>
-      <a href="/" className="menu-item"><i className="fa fa-home" aria-hidden="true"></i> Home</a>
+    <Menu right={ true } styles={ styles } isOpen={false}>
+      <RadiumLink to="/training" className="menu-item"><i className="fa fa-code" aria-hidden="true" /> Training</RadiumLink>
+      <RadiumLink to="/books" className="menu-item"><i className="fa fa-book" aria-hidden="true" /> Books</RadiumLink>
+      <RadiumLink to="/posts" className="menu-item"><i className="fa fa-commenting" aria-hidden="true" /> Posts</RadiumLink>
+      <RadiumLink to="/about" className="menu-item"><i className="fa fa-info-circle" aria-hidden="true" /> About</RadiumLink>
+      <RadiumLink to="/register" className="menu-item"><i className="fa fa-user-plus" aria-hidden="true" /> Register</RadiumLink>
+      <RadiumLink to="/" className="menu-item"><i className="fa fa-home" aria-hidden="true" /> Home</RadiumLink>
     </Menu>
   );
 }
@@ -35,13 +49,13 @@ function DesktopMenu() {
   return (
     <div className="app-header--navigation medium-8 columns">
       <span className="non-register">
-        <a href="/training" className="menu-item">Training</a>
-        <a href="/books" className="menu-item">Books</a>
-        <a href="/posts" className="menu-item">Posts</a>
-        <a href="/about" className="menu-item">About</a>
+        <Link to="/training" className="menu-item">Training</Link>
+        <Link to="/books" className="menu-item">Books</Link>
+        <Link to="/posts" className="menu-item">Posts</Link>
+        <Link to="/about" className="menu-item">About</Link>
       </span>
       <span className="register">
-        <a href="/register" className="menu-item">Register</a>
+        <Link to="/register" className="menu-item">Register</Link>
       </span>
     </div>
   );
