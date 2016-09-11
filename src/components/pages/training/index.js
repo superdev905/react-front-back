3f9f35d0ef197a553ee46a7b62a3b9ffd65b9786
superdev905 <superdev905@gmail.com>
2016-09-10 17:37:16 -0700

    Add available workshops mobile

--- src/components/pages/training/index.js
+++ src/components/pages/training/index.js
@@ -27,16 +27,18 @@ function Tagline() {
 function Workshop1() {
 	return (
 		<div className="training--workshop1">
-			<div className="training--workshop1--date">
-        <Link to="/training/react-2016">Sat, April 23 at 9:00 am, San Francisco</Link>
-			</div>
-			<div className="master--react--redux--1"><span>React 2016:</span></div>
-      <div className="master--react--redux"><span>Master React, Redux,</span></div>
-      <div className="master--react--redux"><span>Immutable.js and</span></div>
-      <div className="master--react--redux"><span>Webpack</span></div>
-      <p className="training--workshop--authors">With Freddy Rangel & Craig Condon</p>
-      <div className="training--view">
-        <Button url="/training/react-2016">View Course <i className="fa fa-long-arrow-right" aria-hidden="true"></i></Button>
+      <div className="training--workshop1--container">
+  			<div className="training--workshop1--date">
+          <Link to="/training/react-2016">Sat, April 23 at 9:00 am, San Francisco</Link>
+  			</div>
+  			<div className="master--react--redux--1"><span>React 2016:</span></div>
+        <div className="master--react--redux"><span>Master React, Redux,</span></div>
+        <div className="master--react--redux"><span>Immutable.js and</span></div>
+        <div className="master--react--redux"><span>Webpack</span></div>
+        <p className="training--workshop--authors">With Freddy Rangel & Craig Condon</p>
+        <div className="training--view">
+          <Button url="/training/react-2016">View Course <i className="fa fa-long-arrow-right" aria-hidden="true"></i></Button>
+        </div>
       </div>
 		</div>
 	)
@@ -45,14 +47,16 @@ function Workshop1() {
 function Workshop2() {
   return (
     <div className="training--workshop2">
-      <div className="training--workshop2--date">
-        <Link to="/training/react-and-d3">Sun, May 8 at 9:00 am, San Francisco</Link>
-      </div>
-      <div className="training--workshop2--react"><span>React and D3</span></div>
-      <p className="training--workshop2--authors">With Swizec Teller & Freddy Rangel</p>
-      <div className="training--workshop2--view">
-        <Button url="/training/react-and-d3">View Course <i className="fa fa-long-arrow-right" aria-hidden="true"></i></Button>
-      </div>
+      <div className="training--workshop2--container">
+        <div className="training--workshop2--date">
+          <Link to="/training/react-and-d3">Sun, May 8 at 9:00 am, San Francisco</Link>
+        </div>
+        <div className="training--workshop2--react"><span>React and D3</span></div>
+        <p className="training--workshop2--authors">With Swizec Teller & Freddy Rangel</p>
+        <div className="training--workshop2--view">
+          <Button url="/training/react-and-d3">View Course <i className="fa fa-long-arrow-right" aria-hidden="true"></i></Button>
+        </div>
+      </div>    
     </div>
   );
 }
